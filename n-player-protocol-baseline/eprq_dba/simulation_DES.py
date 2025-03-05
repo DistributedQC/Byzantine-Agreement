e373b41ffec3e3806031866160aa934828ec6753
Mayank Bhatia <mikkubhatia@gmail.com>
2025-03-05 17:46:26 -0600

    revert to correct baseline

--- n-player-protocol-baseline/eprq_dba/simulation_DES.py
+++
@@ -1,4 +0,0 @@
-import random
-from eprq_dba.players import Commander, Lieutenant, EvidenceBundle, InitialEvidence, IntermediaryEvidence
-from eprq_dba.config import COMMANDER_NAME, LIEUTENANT_NAMES, M, N, COMMANDER_IS_TRAITOR, TRAITOR_INDICES, LOYAL_COMMANDER_ORDER
-
